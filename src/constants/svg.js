b81947b82ce10fa6048d11ff62a673b05007fdb3
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-05-19 21:58:35 +0300

    Add Form: new common component Form #11

--- src/constants/svg.js
+++ src/constants/svg.js
@@ -1,9 +1,11 @@
 import arrowForButton from "/public/images/svg/arrowBt.svg";
+import iconDropdown from "/public/images/svg/iconDropdown.svg";
 import yourTourWhite from "/public/images/svg/yourTourWhite.svg";
 import yourTourBlack from "/public/images/svg/yourTourBlack.svg";
 
 export default {
     arrowForButton,
+    iconDropdown,
     yourTourWhite,
     yourTourBlack,
 };
